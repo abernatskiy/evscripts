da3a6dc54f7ff3ba0c224c634f11df1c0d686155
Anton Bernatskiy (alienware) <abernats@uvm.edu>
2014-10-15 21:39:51 -0500

    Movie support is finished, but OpenGL bug remains to be fixed

--- movie.sh
+++ movie.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Usage: ./movie.sh <forceGain> <sensorGain> <network>
+
+# On most monitors, you will need to modity Bullet's
+# source code to get this running. Change line 81 of
+# Demos/OpenGL/GlutStuff.cpp to take 0s instead of
+# width/2, height/2, then cd to build3/gmake
+# (assuming you use bullet3) and rebuild the library
+
 source vars
 
 CLIENT_MAIN=$CLIENT_DIR/tadrosim-graphics
@@ -15,7 +23,10 @@ function spawnGraphicalClient(){
   cd "$CLIENT_DIR"
   make clean > "$CLIENT_MAKE_LOG" 2>&1
   make tadrosim-graphics MORECFLAGS="$1" >> "$CLIENT_MAKE_LOG" 2>&1
+	echo making tadrosim-graphics MORECFLAGS="$1" ...
+#  make tadrosim-graphics MORECFLAGS="$1"
 	./tadrosim-graphics in out > "$CLIENT_LOG" &
+#	./tadrosim-graphics in out &
 	CLIENT_PID=$!
 	cd "$CURDIR"
 #	echo Spawned a client process, PID $CLIENT_PID
@@ -32,7 +43,21 @@ function waitForPID(){
 }
 
 spawnGraphicalClient "-DFORCE_GAIN=${1}f -DSENSOR_GAIN=${2}f"
+echo sending `echo $@ | cut -d' ' -f3-` to in
 echo $@ | cut -d' ' -f3- > $CLIENT_DIR/in
+sleep 1
+CLIENT_WINDOWID=`wmctrl -l | grep "Bullet Physics Demo. http:\/\/bulletphysics.com" | cut -d' ' -f1`
+recordmydesktop --windowid $CLIENT_WINDOWID --no-sound &
+RMD_PID=$!
 
-cat $CLIENT_DIR/out
+echo Im here
+
+cat $CLIENT_DIR/out &
+
+sleep 5
+
+echo $CLIENT_PID $RMD_PID
+
 kill $CLIENT_PID
+
+kill $RMD_PID
