9b30b42e90735a0a8732855077d4dfa473f992d3
Anton Bernatskiy <abernats@uvm.edu>
2015-01-18 01:50:01 -0500

    Q file concatenation (paste) added

--- modularityProcessors/computeQ.sh
+++ modularityProcessors/computeQ.sh
@@ -11,4 +11,6 @@ for file in ${CURDIR}/bestIndividual*.log; do
 	cat $file | tail -n +2 | cut -d ' ' -f3- | $QCOMPUTER | cut -d ' ' -f2 > ${file}.q
 done
 
+paste -d ' ' *.log.q > qvalues
+
 cd $CURDIR
