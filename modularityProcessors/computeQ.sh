64590db7629e5ced987cc0b2e9662278dde9967a
Anton Bernatskiy <abernats@uvm.edu>
2015-01-17 20:22:38 -0500

    Improved computeQ script
    Now network IDs are not saved since there's no use for them

--- modularityProcessors/computeQ.sh
+++ modularityProcessors/computeQ.sh
@@ -8,7 +8,7 @@ QCOMPUTER="${WORKDIR}massQ.sh"
 cd $WORKDIR
 
 for file in ${CURDIR}/bestIndividual*.log; do
-	cat $file | tail -n +2 | cut -d ' ' -f3- | head -50 | $QCOMPUTER > ${file}.q
+	cat $file | tail -n +2 | cut -d ' ' -f3- | $QCOMPUTER | cut -d ' ' -f2 > ${file}.q
 done
 
 cd $CURDIR
