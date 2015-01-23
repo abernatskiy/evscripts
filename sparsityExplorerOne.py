c8b4e13bac5a2863c0e1a7eec2507ccb7c5c36e8
Anton Bernatskiy <abernats@uvm.edu>
2015-01-23 00:01:32 -0500

    Q value computation is now performed online
    Other news:
     - most important workers now do mot rely on absolute paths to
       the home directory
     - added computeFileQ.sh - script which automatically creates
       unique named pipes to store the intermediate data in Q evals
    
    	new file:   modularityProcessors/computeFileQ.sh
    	modified:   sparsityExplorerOne.py
    	modified:   sparsityExplorerOne.sh

--- sparsityExplorerOne.py
+++ sparsityExplorerOne.py
@@ -1,8 +1,9 @@
 import subprocess
 import sys
+from os.path import expanduser
 
-home = '/users/a/b/abernats/'
-file = open(home + 'evscripts/randints1421128240.dat', 'r')
+home = expanduser("~")
+file = open(home + '/evscripts/randints1421128240.dat', 'r')
 seeds = []
 for line in file:
 	seeds.append(int(line))
@@ -13,10 +14,12 @@ indivPipeName = '/tmp/individuals' + str(oneTrueSeed) + '.log'
 subprocess.call(['/usr/bin/mkfifo', evalPipeName])
 subprocess.call(['/usr/bin/mkfifo', indivPipeName])
 
-client = subprocess.Popen([home + 'anaconda/bin/python2.7', home + 'eswclient/runEvaluator.py', indivPipeName, evalPipeName])
-subprocess.call([home + 'anaconda/bin/python2.7', home + 'evs/main.py', evalPipeName, indivPipeName, str(oneTrueSeed)])
+client = subprocess.Popen([home + '/anaconda/bin/python2.7', home + '/eswclient/runEvaluator.py', indivPipeName, evalPipeName])
+subprocess.call([home + '/anaconda/bin/python2.7', home + '/evs/main.py', evalPipeName, indivPipeName, str(oneTrueSeed)])
 
 client.send_signal(subprocess.signal.SIGTERM)
 
 subprocess.call(['/bin/rm', evalPipeName])
 subprocess.call(['/bin/rm', indivPipeName])
+
+subprocess.call(['/bin/bash', home + '/evscripts/modularityProcessors/computeFileQ.sh', 'bestIndividual' + str(oneTrueSeed) + '.log'])
--- sparsityExplorerOne.sh
+++ sparsityExplorerOne.sh
@@ -1,7 +1,7 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=01:00:00
 #PBS -N myjob
-#PBS -j oe   
+#PBS -j oe
 #PBS -m bea
 
 cd $PBS_O_WORKDIR
