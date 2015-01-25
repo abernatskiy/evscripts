a4020230beb955aff5b9d535f1735b095d2727bc
Anton Bernatskiy (cluster) <abernats@uvm.edu>
2015-01-24 23:57:50 -0500

    Now sparsity explorers perform an entire investigation per run

--- sparsityExplorerOne.py
+++ sparsityExplorerOne.py
@@ -1,7 +1,10 @@
 import subprocess
 import sys
 from os.path import expanduser
+from os.path import isdir
+import os
 
+dirs = [ d for d in os.listdir('.') if isdir(d) ]
 home = expanduser("~")
 file = open(home + '/evscripts/randints1421128240.dat', 'r')
 seeds = []
@@ -9,17 +12,20 @@ for line in file:
 	seeds.append(int(line))
 oneTrueSeed = seeds[int(sys.argv[1])]
 
-evalPipeName = '/tmp/evaluations' + str(oneTrueSeed) + '.log'
-indivPipeName = '/tmp/individuals' + str(oneTrueSeed) + '.log'
+evalPipeName = '/tmp/evaluations' + str(oneTrueSeed) + '.pipe'
+indivPipeName = '/tmp/individuals' + str(oneTrueSeed) + '.pipe'
 subprocess.call(['/usr/bin/mkfifo', evalPipeName])
 subprocess.call(['/usr/bin/mkfifo', indivPipeName])
 
 client = subprocess.Popen([home + '/anaconda/bin/python2.7', home + '/eswclient/runEvaluator.py', indivPipeName, evalPipeName])
-subprocess.call([home + '/anaconda/bin/python2.7', home + '/evs/mainConfig.py', evalPipeName, indivPipeName, str(oneTrueSeed), sys.argv[2]])
+
+for dir in dirs:
+	os.chdir(dir)
+	subprocess.call([home + '/anaconda/bin/python2.7', home + '/evs/mainConfig.py', evalPipeName, indivPipeName, str(oneTrueSeed), sys.argv[2]])
+	subprocess.call(['/bin/bash', home + '/evscripts/modularityProcessors/computeFileQ.sh', 'bestIndividual' + str(oneTrueSeed) + '.log'])
+	os.chdir('..')
 
 client.send_signal(subprocess.signal.SIGTERM)
 
 subprocess.call(['/bin/rm', evalPipeName])
 subprocess.call(['/bin/rm', indivPipeName])
-
-subprocess.call(['/bin/bash', home + '/evscripts/modularityProcessors/computeFileQ.sh', 'bestIndividual' + str(oneTrueSeed) + '.log'])
