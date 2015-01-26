fecad78db065c3ba3a6ed2fa66a007592515e30b
Anton Bernatskiy <abernats@uvm.edu>
2015-01-26 15:30:04 -0500

    Some creative chaos in plotFitnessTimeSeries.py + density-measuring oneliner

--- modularityProcessors/plotFitnessTimeSeries.py
+++ modularityProcessors/plotFitnessTimeSeries.py
@@ -25,15 +25,21 @@ def plotFitnessTS(filename, plotColor, plotStd=True):
 
 #plotFitnessTS('fg16.0sg4.0cc0.0_fitnessTimeSeries.dat', 'red')
 
-colors = ['red', 'green', 'cyan', 'blue', 'violet']
+colors = ['black', 'red', 'yellow', 'green', 'cyan', 'blue', 'violet']
+#colors = ['red', 'green', 'cyan', 'blue', 'violet']
 colorIdx = 0
 
-for file in ['random_expl0.5_insdel0.9.qvalue', 'random_expl0.5_insdel2.0.qvalue', 'sparse_expl0.5_insdel0.9.qvalue', 'sparse_expl0.5_insdel2.0.qvalue', 'sparse_expl0.5_insdel0.9_newchange.qvalue']:
-	plotFitnessTS(file, colors[colorIdx], plotStd=True)
+files = ['espinosa-soto', 'random_expl0.5_insdel0.5', 'random_expl0.5_insdel1.0', 'random_expl0.5_insdel2.0', 'sparse_expl0.5_insdel0.5', 'sparse_expl0.5_insdel1.0', 'sparse_expl0.5_insdel2.0']
+#files = ['random_expl0.5_insdel0.5', 'random_expl0.5_insdel1.0', 'random_expl0.5_insdel2.0']
+#files = ['sparse_expl0.5_insdel0.5', 'sparse_expl0.5_insdel1.0', 'sparse_expl0.5_insdel2.0']
+#files = ['random_expl0.5_insdel1.0', 'sparse_expl0.5_insdel1.0', 'espinosa-soto']
+for file in files:
+	plotFitnessTS(file + '.qvalue', colors[colorIdx], plotStd=True)
 #	plotFitnessTS(file, colors[colorIdx])
-	colorIdx = (colorIdx + 1) % 5
+	colorIdx = (colorIdx + 1) % 7
 
-plt.legend(loc=1)
+#plt.legend(loc=4) # for fitness
+plt.legend(loc=1) # for Q
 plt.title('Q vs time')
-plt.xlim(0,100)
+plt.xlim(0,2000)
 #plt.show()
