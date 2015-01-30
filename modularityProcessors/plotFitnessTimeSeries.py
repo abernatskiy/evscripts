f950e4db90d6e380c90e70bab12e0ca02e9105c8
Anton Bernatskiy <abernats@uvm.edu>
2015-01-30 02:33:40 -0500

    Some useful comments added to plotFitnessTimeSeries.py

--- modularityProcessors/plotFitnessTimeSeries.py
+++ modularityProcessors/plotFitnessTimeSeries.py
@@ -7,7 +7,9 @@ import fnmatch
 
 def plotFitnessTS(filename, plotColor, plotStd=True):
 	fitnessTS = np.loadtxt(filename)
+#	print fitnessTS
 	fitnessMeanTS = np.mean(fitnessTS, axis=1)
+#	print fitnessMeanTS
 	fitnessStdTS = np.std(fitnessTS, axis=1)*1.96/np.sqrt(float(fitnessTS.shape[1]))
 
 	fitnessLower = fitnessMeanTS - fitnessStdTS
@@ -15,8 +17,11 @@ def plotFitnessTS(filename, plotColor, plotStd=True):
 
 	gens = np.arange(0,len(fitnessMeanTS))
 
-	plt.xlabel('Generations')
+	plt.xlabel('Number of generations')
+
 	plt.ylabel('Fitness')
+#	plt.ylabel('Q')
+#	plt.ylabel('Number of connections')
 
 	if plotStd:
 		plt.plot(gens, fitnessLower, gens, fitnessUpper, color=plotColor, alpha=0.5)
@@ -29,17 +34,19 @@ colors = ['black', 'red', 'yellow', 'green', 'cyan', 'blue', 'violet']
 #colors = ['red', 'green', 'cyan', 'blue', 'violet']
 colorIdx = 0
 
-files = ['espinosa-soto', 'random_expl0.5_insdel0.5', 'random_expl0.5_insdel1.0', 'random_expl0.5_insdel2.0', 'sparse_expl0.5_insdel0.5', 'sparse_expl0.5_insdel1.0', 'sparse_expl0.5_insdel2.0']
+#files = ['espinosa-soto', 'random_expl0.5_insdel0.5', 'random_expl0.5_insdel1.0', 'random_expl0.5_insdel2.0', 'sparse_expl0.5_insdel0.5', 'sparse_expl0.5_insdel1.0', 'sparse_expl0.5_insdel2.0']
 #files = ['random_expl0.5_insdel0.5', 'random_expl0.5_insdel1.0', 'random_expl0.5_insdel2.0']
 #files = ['sparse_expl0.5_insdel0.5', 'sparse_expl0.5_insdel1.0', 'sparse_expl0.5_insdel2.0']
 #files = ['random_expl0.5_insdel1.0', 'sparse_expl0.5_insdel1.0', 'espinosa-soto']
-for file in files:
-	plotFitnessTS(file + '.qvalue', colors[colorIdx], plotStd=True)
+for file in os.listdir('.'):
+	plotFitnessTS(file, colors[colorIdx], plotStd=True)
 #	plotFitnessTS(file, colors[colorIdx])
 	colorIdx = (colorIdx + 1) % 7
 
-#plt.legend(loc=4) # for fitness
-plt.legend(loc=1) # for Q
-plt.title('Q vs time')
-plt.xlim(0,2000)
+plt.legend(loc=4) # for fitness
+#plt.legend(loc=1) # for Q
+#plt.title('Average density vs generation')
+#plt.title('Average Q vs generation')
+plt.title('Average fitness vs generation')
+plt.xlim(0,100)
 #plt.show()
