ecf718fb6ab83ea63f92ff68b928dbbc7e49b4b5
Anton Bernatskiy (cluster) <abernats@uvm.edu>
2015-02-01 18:17:22 -0500

    Client is now restarted every time we change workdir in sparsityExplorerOne.sh

--- sparsityExplorerOne.py
+++ sparsityExplorerOne.py
@@ -4,6 +4,8 @@ from os.path import expanduser
 from os.path import isdir
 import os
 
+eswclient = 'eswclient'
+
 dirs = [ d for d in os.listdir('.') if isdir(d) ]
 home = expanduser("~")
 file = open(home + '/evscripts/randints1421128240.dat', 'r')
@@ -17,15 +19,13 @@ indivPipeName = '/tmp/individuals' + str(oneTrueSeed) + '_pid' + str(os.getpid()
 subprocess.call(['/usr/bin/mkfifo', evalPipeName])
 subprocess.call(['/usr/bin/mkfifo', indivPipeName])
 
-client = subprocess.Popen([home + '/anaconda/bin/python2.7', home + '/eswclient/runEvaluator.py', indivPipeName, evalPipeName])
-
 for dir in dirs:
 	os.chdir(dir)
+	client = subprocess.Popen([home + '/anaconda/bin/python2.7', home + '/' + eswclient + '/runEvaluator.py', indivPipeName, evalPipeName])
 	subprocess.call([home + '/anaconda/bin/python2.7', home + '/evs/mainConfig.py', evalPipeName, indivPipeName, str(oneTrueSeed), sys.argv[2]])
+	client.send_signal(subprocess.signal.SIGTERM)
 	subprocess.call(['/bin/bash', home + '/evscripts/modularityProcessors/computeFileQ.sh', 'bestIndividual' + str(oneTrueSeed) + '.log'])
 	os.chdir('..')
-
-client.send_signal(subprocess.signal.SIGTERM)
 
 subprocess.call(['/bin/rm', evalPipeName])
 subprocess.call(['/bin/rm', indivPipeName])
