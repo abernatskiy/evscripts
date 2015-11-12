4a663d0286153dde12d8456bcc685b0be9b8fa4a
Anton Bernatskiy (ishigaki) <abernats@uvm.edu>
2015-11-12 16:34:22 -0500

    Uniform sampling experiment now also investigates random search
    The investigation is a part of the effort to determine
    normalization coefficients for LPE.
    
    	modified:   uniformSamplingLPE.py

--- uniformSamplingLPE.py
+++ uniformSamplingLPE.py
@@ -8,41 +8,60 @@ import staticEvsDynamicCeExperiment as sedce
 import shared.grid
 from copy import copy
 
-def _extractFitness(filename):
+def extractFitness(filename):
 	strFitnessVals = subprocess.check_output(['/usr/bin/cut', '-d', ' ', '-f', '1', filename])
 	return np.fromstring(strFitnessVals[6:], sep='\n')
 
-def _readFitnessIncrements(files):
-	fitness = [ _extractFitness(file) for file in files ]
-	return (fitness[1] - fitness[0])/fitness[0]
+def stderr(samples):
+	return np.std(samples)/np.sqrt(float(len(samples)))
 
-def _readFitnessDifferences(files):
-	fitness = [ _extractFitness(file) for file in files ]
-	return np.abs(fitness[1] - fitness[0])/fitness[0]
+def recordEvolvabilityStats(fullCond, fitness, experiment):
+	inc = (fitness[1] - fitness[0])/fitness[0]
+	diff = np.abs(fitness[1] - fitness[0])/fitness[0]
+
+	results = {}
+	results['incMean'] = np.mean(inc)
+	results['incStdErr'] = stderr(inc)
+	experiment.addResultRecord('increments.dat', fullCond, results)
+
+	results = {}
+	results['diffMean'] = np.mean(diff)
+	results['diffStdErr'] = stderr(diff)
+	experiment.addResultRecord('differences.dat', fullCond, results)
+
+def chunks(l, n):
+	'''Yield successive n-sized chunks from l.'''
+	for i in xrange(0, len(l), n):
+		yield l[i:i+n]
+
+def chunkedMaxStats(fitness, numChunks):
+	if len(fitness[0]) % numChunks != 0:
+		raise ValueError('Number of fitness records (' + str(len(fitness)) + ')  must be divisible by the number of chunks (' + str(numChunks) + ')')
+	lenChunk = len(fitness[0])/numChunks
+	fitChunks = chunks(fitness[0], lenChunk)
+	fitMax = map(np.max, fitChunks)
+	return (np.mean(fitMax), np.std(fitMax))
+
+def recordRandomSearchStats(fullCond, fitness, experiment):
+	results = {}
+	for nc in [1, 10, 100, 1000]:
+		mean, std = chunkedMaxStats(fitness, nc)
+		results['mean' + str(nc)] = mean
+		results['std' + str(nc)] = std
+	experiment.addResultRecord('randomSearchStats.dat', fullCond, results)
 
 class uniformSamplingLPEExperiment(sedce.staticEvsDynamicCeExperiment):
 	def processResults(self):
 		def processDir(gridPoint, condPoint, self):
 			fileNameBase = 'population' + str(int(gridPoint['randomSeed'])) + '_gen'
 			files = [ fileNameBase + '0.log', fileNameBase + '1.log' ]
-
-			inc = _readFitnessIncrements(files)
-			diff = _readFitnessDifferences(files)
+			fitness = [ extractFitness(file) for file in files ]
 
 			fullCond = copy(gridPoint)
 			fullCond.update(condPoint)
-			def stderr(samples):
-				return np.std(samples)/np.sqrt(float(len(samples)))
 
-			results = {}
-			results['incMean'] = np.mean(inc)
-			results['incStdErr'] = stderr(inc)
-			self.addResultRecord('increments.dat', fullCond, results)
-
-			results = {}
-			results['diffMean'] = np.mean(diff)
-			results['diffStdErr'] = stderr(diff)
-			self.addResultRecord('differences.dat', fullCond, results)
+			recordEvolvabilityStats(fullCond, fitness, self)
+			recordRandomSearchStats(fullCond, fitness, self)
 		self.executeAtEveryConditionsDir(processDir, (self,), {})
 
 	def evsConfig(self):
@@ -59,12 +78,11 @@ class uniformSamplingLPEExperiment(sedce.staticEvsDynamicCeExperiment):
 						'logPopulation = yes\n')
 
 def initializeExperiment():
-	experiment = uniformSamplingLPEExperiment('uniformSamplingLPE201511103',
+	return uniformSamplingLPEExperiment('uniformSamplingLPE201511103',
 				[{'linearDrag':0.0, 'angularDrag':0.0}, {'linearDrag':0.2, 'angularDrag':0.2}],
 				grid=shared.grid.LogLinGrid([['sensorGain', 'log', 16.0, 4.0, 1, 1], ['forceGain', 'log', 0.8, 4.0, 2, 2]]),
 				pointsPerJob=1,
 				expectedWallClockTime='03:00:00')
-	return experiment
 
 if __name__ == '__main__':
 	e = initializeExperiment()
