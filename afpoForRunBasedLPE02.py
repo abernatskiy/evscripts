3af833485bda13e6b6e0ae3b166a59a6793b0e9b
Anton Bernatskiy (cluster) <abernats@uvm.edu>
2016-01-16 20:25:59 -0500

    Dir number fixed

--- afpoForRunBasedLPE02.py
+++ afpoForRunBasedLPE02.py
@@ -40,7 +40,7 @@ def initializeExperiment():
 	grid = afpoBaseGrid()
 	randSeedsList = tfp.randSeedList(os.path.join(routes.evscriptsHome, 'seedFiles', 'randints1416551751.dat'), size=200)
 	grid *= Grid1d('randomSeeds', randSeedsList[14:21])
-	return afpoForRunBasedLPEExperiment('afpoForRunBasedLPE01',
+	return afpoForRunBasedLPEExperiment('afpoForRunBasedLPE02',
 					[{}],
 					grid=grid,
 					expectedWallClockTime='29:00:00',
