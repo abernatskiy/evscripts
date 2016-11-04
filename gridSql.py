b1ae65ffb7992ff96b10afc553d3599105ab5d4a
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2016-11-04 19:42:18 -0400

    Gridsql done
    	modified:   gridSql.py

--- gridSql.py
+++ gridSql.py
@@ -16,11 +16,11 @@ def _sqlStr(var):
 	else:
 		raise ValueError('Conversion of a variable of unsuitable type to SQL converted')
 
-def grid2sqlite(grid, dbfilename):
+def makeGridTable(grid, dbfilename):
 	with sqlite3.connect(dbfilename) as con:
 		cur = con.cursor()
 
-		tableDesc = 'Grid(id INT'
+		tableDesc = 'Grid(id INT PRIMARY KEY'
 		examplePoint = grid[0]
 		paramNames = examplePoint.keys()
 		for paramName in paramNames:
@@ -32,8 +32,55 @@ def grid2sqlite(grid, dbfilename):
 			vstrs = [ str(i) ] + [ _sqlStr(point[paramName]) for paramName in paramNames ]
 			cur.execute('INSERT INTO Grid VALUES (' + ', '.join(vstrs) + ');')
 
+def gridIdsFromSqlite(dbfilename):
+	with sqlite3.connect(dbfilename) as con:
+		cur = con.cursor()
+		cur.execute('SELECT id FROM Grid;')
+		idTuples = cur.fetchall()
+	return [ id for id, in idTuples ]
+
 def makeGridQueueTable(dbfilename):
-	#with sqlite3.connect(dbfilename) as con:
-	#	cur = con.cursor()
-	#	cur.execute('CREATE TABLE GridCoverage(FOREIGN KEY(id) REFERENCES Grid(id), ')
-	pass
+	ids = gridIdsFromSqlite(dbfilename)
+	with sqlite3.connect(dbfilename) as con:
+		cur = con.cursor()
+		cur.execute('PRAGMA foreign_keys=ON;')
+		cur.execute('CREATE TABLE GridQueue(id INT, inWorks BOOL, done BOOL, FOREIGN KEY(id) REFERENCES Grid(id));')
+		for id in ids:
+			cur.execute('INSERT INTO GridQueue VALUES (' + str(id) + ', 0, 0);')
+
+def requestPointFromGridQueue(dbfilename):
+	'''Finds a point that is neither worked upon nor done, marks it as worked upon
+	   and returns the parameter dictionary. If the point is not found, None is
+	   returned.
+	'''
+	with sqlite3.connect(dbfilename) as con:
+		con.text_factory = str
+		cur = con.cursor()
+		cur.execute('SELECT id FROM GridQueue WHERE NOT inWorks AND NOT done LIMIT 1;')
+		pointIdList = cur.fetchall()
+		if len(pointIdList) == 1: # If there is still a point that hasn't been processed
+			pointId, = pointIdList[0]
+			# mark it as being in works
+			cur.execute('UPDATE GridQueue SET inWorks=1 WHERE id=' + str(pointId) + ';')
+			# fetch and return the grid parameters
+			cur.execute('SELECT * FROM Grid WHERE id=' + str(pointId) + ';')
+			vals = cur.fetchall()
+			dict = {}
+			for idx, col in enumerate(cur.description):
+				dict[col[0]] = vals[0][idx]
+			id = dict.pop('id')
+			return id, dict
+		elif len(pointIdList) == 0:
+			return None
+		else:
+			raise ValueError('SELECT query with LIMIT 1 returned more than one result')
+
+def reportSuccessOnPoint(dbfilename, id):
+	with sqlite3.connect(dbfilename) as con:
+		cur = con.cursor()
+		cur.execute('UPDATE GridQueue SET inWorks=0, done=1 WHERE id=' + str(id) + ';')
+
+def reportFailureOnPoint(dbfilename, id):
+	with sqlite3.connect(dbfilename) as con:
+		cur = con.cursor()
+		cur.execute('UPDATE GridQueue SET inWorks=0 WHERE id=' + str(id) + ';')
