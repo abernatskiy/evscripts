8f8e355411871ae3897a65ab804822abc835daa1
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2016-11-07 14:17:09 -0500

    Multiple passes supported by gridSql
    	modified:   gridSql.py

--- gridSql.py
+++ gridSql.py
@@ -39,14 +39,14 @@ def gridIdsFromSqlite(dbfilename):
 		idTuples = cur.fetchall()
 	return [ id for id, in idTuples ]
 
-def makeGridQueueTable(dbfilename):
+def makeGridQueueTable(dbfilename, passes=1):
 	ids = gridIdsFromSqlite(dbfilename)
 	with sqlite3.connect(dbfilename) as con:
 		cur = con.cursor()
 		cur.execute('PRAGMA foreign_keys=ON;')
-		cur.execute('CREATE TABLE GridQueue(id INT, inWorks BOOL, done BOOL, FOREIGN KEY(id) REFERENCES Grid(id));')
+		cur.execute('CREATE TABLE GridQueue(id INT, passesRequested INT, passesDone INT, passesFailed INT, inWorks BOOL, FOREIGN KEY(id) REFERENCES Grid(id));')
 		for id in ids:
-			cur.execute('INSERT INTO GridQueue VALUES (' + str(id) + ', 0, 0);')
+			cur.execute('INSERT INTO GridQueue VALUES ({}, {}, 0, 0, 0);'.format(id, passes))
 
 def requestPointFromGridQueue(dbfilename):
 	'''Finds a point that is neither worked upon nor done, marks it as worked upon
@@ -56,20 +56,21 @@ def requestPointFromGridQueue(dbfilename):
 	with sqlite3.connect(dbfilename) as con:
 		con.text_factory = str
 		cur = con.cursor()
-		cur.execute('SELECT id FROM GridQueue WHERE NOT inWorks AND NOT done LIMIT 1;')
+		cur.execute('SELECT id, passesDone FROM GridQueue WHERE NOT inWorks AND passesRequested > passesDone LIMIT 1;')
 		pointIdList = cur.fetchall()
 		if len(pointIdList) == 1: # If there is still a point that hasn't been processed
-			pointId, = pointIdList[0]
+			pointId, passesDone = pointIdList[0]
+			curPass = passesDone + 1
 			# mark it as being in works
-			cur.execute('UPDATE GridQueue SET inWorks=1 WHERE id=' + str(pointId) + ';')
+			cur.execute('UPDATE GridQueue SET inWorks=1 WHERE id={};'.format(pointId))
 			# fetch and return the grid parameters
-			cur.execute('SELECT * FROM Grid WHERE id=' + str(pointId) + ';')
+			cur.execute('SELECT * FROM Grid WHERE id={};'.format(pointId))
 			vals = cur.fetchall()
 			dict = {}
 			for idx, col in enumerate(cur.description):
 				dict[col[0]] = vals[0][idx]
 			id = dict.pop('id')
-			return id, dict
+			return id, curPass, dict
 		elif len(pointIdList) == 0:
 			return None
 		else:
@@ -78,9 +79,9 @@ def requestPointFromGridQueue(dbfilename):
 def reportSuccessOnPoint(dbfilename, id):
 	with sqlite3.connect(dbfilename) as con:
 		cur = con.cursor()
-		cur.execute('UPDATE GridQueue SET inWorks=0, done=1 WHERE id=' + str(id) + ';')
+		cur.execute('UPDATE GridQueue SET inWorks=0, passesDone=passesDone+1 WHERE id={};'.format(id))
 
 def reportFailureOnPoint(dbfilename, id):
 	with sqlite3.connect(dbfilename) as con:
 		cur = con.cursor()
-		cur.execute('UPDATE GridQueue SET inWorks=0 WHERE id=' + str(id) + ';')
+		cur.execute('UPDATE GridQueue SET inWorks=0, passesFailed=passesFailed+1 WHERE id={};'.format(id))
